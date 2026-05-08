a6be6aa83fc6d6636d8d3a00cd05981bb721e9b7
muysengly-itc <muy.sengly@itc.edu.kh>
2026-05-08 09:29:24 +0700

    update

--- lib/Main.dart
+++ lib/Main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,27 +16,56 @@ class MyApp extends StatelessWidget {
         colorScheme: .fromSeed(seedColor: Colors.deepPurple), //
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Welcome to Spean Meas Hotel'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String VERSION = '0.0.0+0';
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  void init() async {
+    final info = await PackageInfo.fromPlatform();
+    VERSION = '${info.version}+${info.buildNumber}';
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title), //
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            //
+            SizedBox(width: 4), //
+            // logo
+            SizedBox(width: 32, height: 32, child: Placeholder()), //
+            SizedBox(width: 4), //
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Spean Meas Hotel"), //
+                Text(VERSION, style: TextStyle(fontSize: 12, color: Colors.blue)), //
+              ],
+            ),
+          ],
+        ),
+        titleSpacing: 0,
+        toolbarHeight: 48,
       ),
       body: Center(
         child: Column(
