2032c55b8600b407154e46a55d36e4cc615bc7d9
Sakitsuu <chansaktry168@gmail.com>
2026-08-26 02:23:26 +0700

    Update

--- lib/features/auth_telegram/main.dart
+++ lib/features/auth_telegram/main.dart
@@ -68,7 +68,7 @@ class _Main_State extends State<Main_> {
         child: Text('Auth Telegram'),
         onPressed: () async {
           try {
-            final url = Uri.parse('https://trychansak.1riel.com/telegram');
+            final url = Uri.parse('https://trychansak.1riel.com/telegram_auth');
             if (await canLaunchUrl(url)) {
               await launchUrl(url, mode: LaunchMode.externalApplication);
             } else {
